80556c177dbec33cb807239de0848e9363376fef
angelorubin <angelorubin@gmail.com>
2024-01-17 17:04:32 -0300

    Implementando pagina de eventos

--- src/app/events/[id]/page.tsx
+++ src/app/events/[id]/page.tsx
@@ -3,6 +3,7 @@ import { redirect } from "next/navigation"
 import Search from "@/app/components/site/Search"
 import useSWR from 'swr'
 import * as http from '@/utils/api/site'
+import Loading from '@/app/components/loading'
 
 type EventProps = {
   params: {
@@ -10,43 +11,39 @@ type EventProps = {
   }
 }
 
-const fetcher = (...args) => http.retrieveEvent(...args).then(res => res)
+const retrieveEvent = (...args) => http.retrieveEvent(...args).then(res => res)
 
 export default function Page({ params }: EventProps) {
-  const { data } = useSWR(``, fetcher)
+  const { data, error, isLoading } = useSWR(`${params.id}`, retrieveEvent)
 
-  const event = {
-    status: true,
-    id: 1,
-    title: 'test',
-    description: 'test'
+  if (error) {
+    return redirect("/")
   }
 
-  if (!event || !event.status) {
-    return redirect("/")
+  if (isLoading) {
+    return (
+      <Loading />
+    )
   }
 
   return (
     <div className="flex h-screen w-full justify-center items-center">
       <div className="flex-col w-1/3">
         <main className="flex-col">
-
           <header className="flex-col gap-5 justify-center items-center">
-            <h1>TESTE</h1>
-            <pre className="text-white">{JSON.stringify(data, null, 2)}</pre>
             <h1 className="text-yellow-600 text-5xl text-center mt-2">
               Amigo Secreto
             </h1>
             <h1 className="text-white text-md mb-5 mt-2 text-center">
-              {event.title}
+              {data.event.title}
             </h1>
-            <p className="text-center text-xs mb-3">{event.description}</p>
+            <p className="text-center text-xs mb-3">
+              {data.event.description}
+            </p>
           </header>
         </main>
 
-        {/*
-        <Search id={event.id} />
-        */}
+        <Search id={data.event.id} />
 
         <footer className="text-center text-sm mt-3">
           © Criado por Angelo Rubin - 2024
